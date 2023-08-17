d2d056338b1c835c68590a28cb89d0612574fdd0
ASUS <152153431@qq.com>
2023-08-17 13:52:02 +0800

    测试

--- 自动上传.bat
+++ 自动上传.bat
@@ -4,19 +4,15 @@ echo.
 echo                                      GitHub自动上传脚本
 echo ===================================================================================
 echo.
-
 set /p change=请输入commit名：
 echo.
 cd "%USERPROFILE%\Desktop\notes"
-
 git pull
 git add .
 git commit -m %change%
 git push origin master
-
 echo.
 echo ===================================================================================
 echo                                      更新完毕
 echo.
-
 pause
