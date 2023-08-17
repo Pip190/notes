258567c4cc50f9de6d7e3e85cf41796a4b0787ae
ASUS <152153431@qq.com>
2023-08-17 13:05:28 +0800

    第二次提交

--- 自动上传.bat
+++ 自动上传.bat
@@ -1,12 +1,24 @@
-@echo off
+@@echo off
 chcp 65001
-echo "-------开始上传-------"
-git status
-set  /p  msg=请输入提交注释:
-git add .
-git commit -m %msg%
+echo.
+echo                                      GitHub自动上传脚本
+echo ===================================================================================
+echo.
+
+set /p change=请输入commit名：
+echo.
+cd "%USERPROFILE%\Desktop\notes"
+
 git pull
-git push
-echo 推送成功：【%msg%】
+git add .
+git commit -m %change%
+git push origin master
+
+echo.
+echo ===================================================================================
+echo                                      更新完毕
+echo.
+
+pause
 echo "--------上传完成--------"
 pause
